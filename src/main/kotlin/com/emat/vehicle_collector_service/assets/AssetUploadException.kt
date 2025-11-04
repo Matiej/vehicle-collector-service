987944e15e7a82636cce7aa1a66e09a74b3f2b87
Maciej Wójcik <maciek.wojcik179@gmail.com>
2025-11-04 22:58:16 +0100

    EMAT-003 | fix validation and del endpoint

--- src/main/kotlin/com/emat/vehicle_collector_service/assets/AssetUploadException.kt
+++ src/main/kotlin/com/emat/vehicle_collector_service/assets/AssetUploadException.kt
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus
 
 class AssetUploadException(
     message: String?,
-    val status: HttpStatus?
+    val status: HttpStatus,
+    val code: String = "ASSET_UPLOAD_ERROR"
 ) : Exception(message) {
 
 }
--- src/main/kotlin/com/emat/vehicle_collector_service/infrastructure/storage/LocalStorageService.kt
+++ src/main/kotlin/com/emat/vehicle_collector_service/infrastructure/storage/LocalStorageService.kt
@@ -1,9 +1,14 @@
 package com.emat.vehicle_collector_service.infrastructure.storage
 
+import com.emat.vehicle_collector_service.api.internal.InternalAssetsController
+import com.emat.vehicle_collector_service.assets.AssetUploadException
 import com.emat.vehicle_collector_service.configuration.AppData
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
@@ -13,6 +18,8 @@ import java.nio.file.Path
 class LocalStorageService(
     private val appData: AppData
 ) : StorageService {
+
+    private val log = LoggerFactory.getLogger(StorageService::class.java)
 
     init {
         val path = Path.of(appData.getAssetsDir())
@@ -40,5 +47,30 @@ class LocalStorageService(
             file.copyTo(target.toFile(), overwrite = true)
             storageKeyPath
         }
+    }
+
+    override fun delete(storageKeyPath: String): Mono<Void> {
+        val target = Path.of(appData.getAssetsDir()).resolve(storageKeyPath)
+        val fileName = target.fileName
+        return Mono.defer {
+            try {
+                log.debug("Trying to remove file: {}", fileName)
+                if (!Files.deleteIfExists(target)) {
+                    return@defer Mono.error<Void>(
+                        AssetUploadException("File not found: $storageKeyPath", HttpStatus.NOT_FOUND, "FILE_NOT_FOUND")
+                    )
+                }
+                log.debug("File deleted successfully: {}", fileName)
+                Mono.empty()
+            } catch (e: Exception) {
+                Mono.error(
+                    AssetUploadException(
+                        "Can't delete file: $storageKeyPath",
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        "DELETE_FAILED"
+                    )
+                )
+            }
+        }.subscribeOn(Schedulers.boundedElastic())
     }
 }
